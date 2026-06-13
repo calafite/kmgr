9c1a33fe5d74fa86f035d268ca90aab03bc202ba
calafite <pertinaciouslymedici@gmail.com>
2026-06-13 02:03:30 +0100

    fix: formatting consistency

--- src/cli.rs
+++ src/cli.rs
@@ -2,7 +2,7 @@ use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(name = "kmgr")]
-#[command(author, version, about = " | A fast, lightweight Minecraft CLI Mod Manager written in Rust", long_about = None)]
+#[command(author, version, about = " A fast, lightweight Minecraft CLI Mod Manager written in Rust", long_about = None)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
